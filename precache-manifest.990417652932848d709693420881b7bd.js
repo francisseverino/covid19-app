d4f32774623b13de6054c9328f37881743b60c41
severinoguzmanf@moravian.edu <severinoguzmanf@moravian.edu>
2020-03-31 13:39:51 -0400

    Updates

---
+++ precache-manifest.990417652932848d709693420881b7bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bfb5aa5bd4396cf376b4b10d17b075",
+    "url": "/covid19-app/index.html"
+  },
+  {
+    "revision": "6bf1bd73b43da28f00b6",
+    "url": "/covid19-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "af3b3e1351313ff8139f",
+    "url": "/covid19-app/static/css/main.648de6fb.chunk.css"
+  },
+  {
+    "revision": "6bf1bd73b43da28f00b6",
+    "url": "/covid19-app/static/js/2.858ca4cf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19-app/static/js/2.858ca4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3b3e1351313ff8139f",
+    "url": "/covid19-app/static/js/main.28c59850.chunk.js"
+  },
+  {
+    "revision": "0ba4031a4a93f243aef1",
+    "url": "/covid19-app/static/js/runtime-main.235d0bb4.js"
+  },
+  {
+    "revision": "31430f64d2f8410a196bca8178ec179f",
+    "url": "/covid19-app/static/media/bg-image.31430f64.jpg"
+  }
+]);
