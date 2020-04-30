5f1db621c55a166306c7a0a2f94d1573f62f91c3
severinoguzmanf@moravian.edu <severinoguzmanf@moravian.edu>
2020-04-29 21:29:16 -0400

    Added search by form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import "./App.css";
-import API from "./config/Api";
-import { Row, Col, Button, Modal, Table, Figure, FormControl } from "react-bootstrap";
+import React, { Component } from 'react';
+import './App.css';
+import API from './config/Api';
+import { Row, Col, Button, Modal, Table, Figure, FormControl } from 'react-bootstrap';
 
 const api = new API();
 
@@ -9,29 +9,58 @@ class Counter extends Component {
   state = {
     global: {},
     byCountry: [],
+    filteredCountries: [],
     byStates: [],
+    filteredStates: [],
     showByCountry: false,
-    showByStates: false
+    showByStates: false,
   };
 
   componentDidMount() {
     //Populate all the needed data to the state
     api.getGlobalTotals().then(response => {
       this.setState({
-        global: response
+        global: response,
       });
     });
     api.getCountries().then(response => {
       this.setState({
-        byCountry: response
+        byCountry: response,
+        filteredCountries: response,
       });
     });
     api.getStates().then(response => {
       this.setState({
-        byStates: response
+        byStates: response,
+        filteredStates: response,
       });
     });
   }
+
+  //Modals Togglers
+  closeByCountry = () => {
+    this.setState({
+      showByCountry: false,
+    });
+  };
+
+  showByCountry = () => {
+    this.setState({
+      showByCountry: true,
+    });
+  };
+
+  closeByStates = () => {
+    this.setState({
+      showByStates: false,
+    });
+  };
+
+  showByStates = () => {
+    this.setState({
+      showByStates: true,
+    });
+  };
 
   formatNumber = value => {
     //i.e. 1324 -> 1,324
@@ -41,50 +70,50 @@ class Counter extends Component {
   render() {
     const { global } = this.state;
     return (
-      <div className="bgimg">
-        <div className="bg-layout">
-          <div className="middle">
-            <div className="content">
-              <h2 className="mb-5 font-weight-light">COVID-19 CORONAVIRUS PANDEMIC</h2>
+      <div className='bgimg'>
+        <div className='bg-layout'>
+          <div className='middle'>
+            <div className='content'>
+              <h2 className='mb-5 font-weight-light'>COVID-19 CORONAVIRUS PANDEMIC</h2>
               <br />
-              <Row className="show-grid content-space" float="center">
+              <Row className='show-grid content-space' float='center'>
                 <Col>
-                  <h1 className="global-cases-total text-info font-weight-lighter">
+                  <h1 className='global-cases-total text-info font-weight-lighter'>
                     {this.formatNumber(global.cases)}
                   </h1>
-                  <h2 className="font-weight-light">Coronavirus Cases</h2>
+                  <h2 className='font-weight-light'>Coronavirus Cases</h2>
                 </Col>
 
                 <Col>
-                  <h1 className="global-cases-total text-danger font-weight-lighter">
+                  <h1 className='global-cases-total text-danger font-weight-lighter'>
                     {this.formatNumber(global.deaths)}
                   </h1>
-                  <h2 className="font-weight-light">Deaths</h2>
+                  <h2 className='font-weight-light'>Deaths</h2>
                 </Col>
 
                 <Col>
-                  <h1 className="global-cases-total text-success font-weight-lighter">
+                  <h1 className='global-cases-total text-success font-weight-lighter'>
                     {this.formatNumber(global.recovered)}
                   </h1>
-                  <h2 className="font-weight-light">Recovered</h2>
+                  <h2 className='font-weight-light'>Recovered</h2>
                 </Col>
 
                 <Col>
-                  <h1 className="global-cases-total text-warning font-weight-lighter">
+                  <h1 className='global-cases-total text-warning font-weight-lighter'>
                     {this.formatNumber(global.active)}
                   </h1>
-                  <h2 className="font-weight-light">Active Cases</h2>
+                  <h2 className='font-weight-light'>Active Cases</h2>
                 </Col>
               </Row>
-              <Button className=" m-5" variant="outline-light" size="lg" onClick={this.showByCountry}>
+              <Button className=' m-5' variant='outline-light' size='lg' onClick={this.showByCountry}>
                 View by Country
               </Button>
-              <Button className="m-5" variant="outline-light" size="lg" onClick={this.showByStates}>
+              <Button className='m-5' variant='outline-light' size='lg' onClick={this.showByStates}>
                 View by USA States
               </Button>
             </div>
           </div>
-          <div className="bottomleft"></div>
+          <div className='bottomleft'></div>
         </div>
         {this.StatesModal()}
         {this.CountryModal()}
@@ -92,40 +121,41 @@ class Counter extends Component {
     );
   }
 
-  closeByCountry = () => {
-    this.setState({
-      showByCountry: false
-    });
+  _filterState = (query, data) => {
+    if (!query || (query && !query.trim())) {
+      return data;
+    }
+    const regex = new RegExp(`${query.trim()}`, 'i');
+    return data.filter(value => value.state.search(regex) >= 0);
   };
 
-  showByCountry = () => {
+  handleStateChange = event => {
+    const filtered = this._filterState(event.target.value, this.state.byStates);
     this.setState({
-      showByCountry: true
-    });
-  };
-
-  closeByStates = () => {
-    this.setState({
-      showByStates: false
-    });
-  };
-
-  showByStates = () => {
-    this.setState({
-      showByStates: true
+      filteredStates: filtered,
+      stateQuery: event.target.value,
     });
   };
 
   StatesModal = () => {
-    const { byStates, showByStates } = this.state;
+    const { showByStates, stateQuery, filteredStates } = this.state;
     return (
       <>
-        <Modal show={showByStates} onHide={this.closeByStates} scrollable={true} size="lg" centered>
-          <Modal.Header closeButton className="bg-dark border-0">
-            <Modal.Title className="text-light">Cases By State in the USA</Modal.Title>
+        <Modal show={showByStates} onHide={this.closeByStates} scrollable={true} size='lg' centered>
+          <Modal.Header closeButton className='bg-dark border-0'>
+            <div class='col-sm-3'>
+              <FormControl
+                type='text'
+                placeholder='Search By State'
+                className='mr-sm-2'
+                value={stateQuery}
+                onChange={this.handleStateChange}
+              />
+            </div>
+            <Modal.Title className='text-light'>Cases By State in the USA</Modal.Title>
           </Modal.Header>
-          <Modal.Body className="bg-dark">
-            <Table striped bordered hover variant="dark">
+          <Modal.Body className='bg-dark'>
+            <Table striped bordered hover variant='dark'>
               <thead>
                 <tr>
                   <th>State</th>
@@ -137,7 +167,7 @@ class Counter extends Component {
                 </tr>
               </thead>
               <tbody>
-                {byStates.map(state => (
+                {filteredStates.map(state => (
                   <tr>
                     <td>{state.state}</td>
                     <td>{this.formatNumber(state.cases)}</td>
@@ -155,30 +185,42 @@ class Counter extends Component {
     );
   };
 
-  handleChange = event => {
+  handleCountryChange = event => {
+    const filtered = this._filterCountry(event.target.value, this.state.byCountry);
     this.setState({
-      search: event.target.value
+      filteredCountries: filtered,
+      countryQuery: event.target.value,
     });
   };
 
+  _filterCountry = (query, data) => {
+    if (!query || (query && !query.trim())) {
+      return data;
+    }
+    const regex = new RegExp(`${query.trim()}`, 'i');
+    return data.filter(value => value.country.search(regex) >= 0);
+  };
+
   CountryModal = () => {
-    const { byCountry, showByCountry, search } = this.state;
+    const { showByCountry, countryQuery, filteredCountries } = this.state;
 
     return (
       <>
-        <Modal show={showByCountry} onHide={this.closeByCountry} scrollable={true} size="xl" centered>
-          <Modal.Header closeButton className="bg-dark border-0">
-            {/* <FormControl
-                type="text"
-                placeholder="Search By Country"
-                className="mr-sm-2"
-                value={search}
-                onChange={this.handleChange}
-              /> */}
-            <Modal.Title className="text-light">Cases By Country</Modal.Title>
+        <Modal show={showByCountry} onHide={this.closeByCountry} scrollable={true} size='xl' centered>
+          <Modal.Header closeButton className='bg-dark border-0'>
+            <div class='col-sm-3'>
+              <FormControl
+                type='text'
+                placeholder='Search By Country'
+                className='mr-sm-2'
+                value={countryQuery}
+                onChange={this.handleCountryChange}
+              />
+            </div>
+            <Modal.Title className='text-light'>Cases By Country</Modal.Title>
           </Modal.Header>
-          <Modal.Body className="bg-dark">
-            <Table striped bordered hover variant="dark">
+          <Modal.Body className='bg-dark'>
+            <Table striped bordered hover variant='dark'>
               <thead>
                 <tr>
                   <th></th>
@@ -195,7 +237,7 @@ class Counter extends Component {
                 </tr>
               </thead>
               <tbody>
-                {byCountry.map(country => (
+                {filteredCountries.map(country => (
                   <tr>
                     <td>{<Figure.Image width={70} src={country.countryInfo.flag} />}</td>
                     <td>{country.country}</td>
