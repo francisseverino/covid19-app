9abb39791b0c77b348271532118736cc148aa16e
Francis Severino <francisjsg2@gmail.com>
2021-01-01 20:10:07 -0500

    refactor api and updated readme

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,30 +1,13 @@
 // Documentation for API https://corona.lmao.ninja/docs/
+
+const request = (endpoint) => 
+  fetch(`https://corona.lmao.ninja/v2/${endpoint}`)
+    .then(response => response.json())
 export default class CarAPI {
-  getGlobalTotals = () => {
-    return fetch('https://corona.lmao.ninja/v2/all')
-      .then(response => {
-        return response.json();
-      })
-      .then(responseJson => {
-        return responseJson;
-      });
-  };
-  getCountries = () => {
-    return fetch('https://corona.lmao.ninja/v2/countries')
-      .then(response => {
-        return response.json();
-      })
-      .then(responseJson => {
-        return responseJson;
-      });
-  };
-  getStates = () => {
-    return fetch('https://corona.lmao.ninja/v2/states')
-      .then(response => {
-        return response.json();
-      })
-      .then(responseJson => {
-        return responseJson;
-      });
-  };
+  getGlobalTotals = () => request('all')
+
+  getCountries = () => request('countries')
+
+  getStates = () => request('states')
+
 }
